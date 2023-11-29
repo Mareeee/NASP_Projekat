1cea6a9e142bb29631829b4dfcea3b711bccce4a
vzdravkovic7 <141317094+vzdravkovic7@users.noreply.github.com>
2023-11-29 23:34:31 +0100

    [UPDATE] Serialization

--- bloom-filter/bloom.go
+++ bloom-filter/bloom.go
@@ -2,13 +2,15 @@ package bloom
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/gob"
 	"fmt"
+	"os"
 )
 
 type BloomFilter struct {
-	m   uint           // duzina bloom  filtera
-	k   uint           // broj hash funkcija
+	m   uint32         // duzina bloom  filtera
+	k   uint32         // broj hash funkcija
 	arr []byte         // niz bajtova
 	hf  []HashWithSeed // niz hash funkcija
 }
@@ -21,7 +23,7 @@ func (bf *BloomFilter) BloomFilterConstructor(expectedElements int, falsePositiv
 }
 
 func (bf *BloomFilter) AddElement(key string) {
-	var i uint
+	var i uint32
 	keyConverted := []byte(key)
 	for i = 0; i < bf.k; i++ {
 		iHashed := bf.hf[i].Hash(keyConverted) % uint64(bf.m)
@@ -30,7 +32,7 @@ func (bf *BloomFilter) AddElement(key string) {
 }
 
 func (bf *BloomFilter) CheckElement(key string) bool {
-	var i uint
+	var i uint32
 	keyConverted := []byte(key)
 	for i = 0; i < bf.k; i++ {
 		iHashed := bf.hf[i].Hash(keyConverted) % uint64(bf.m)
@@ -39,6 +41,53 @@ func (bf *BloomFilter) CheckElement(key string) bool {
 		}
 	}
 	return true
+}
+
+func (bf *BloomFilter) HfLength() int {
+	return len(bf.hf) * 32
+}
+
+func (bf *BloomFilter) ToBytes() []byte {
+	bufferSize := 8 + len(bf.arr) + bf.HfLength()
+	buffer := make([]byte, bufferSize)
+	binary.BigEndian.PutUint32(buffer[0:4], bf.m)
+	copy(buffer[4:len(bf.arr)+4], bf.arr)
+	binary.BigEndian.PutUint32(buffer[len(bf.arr)+4:len(bf.arr)+8], bf.k)
+	offSet := len(bf.arr) + 8
+	for i := 0; i < len(bf.hf); i++ {
+		copy(buffer[offSet:offSet+32], bf.hf[i].Seed)
+		offSet += 32
+	}
+	return buffer
+}
+
+func (bf *BloomFilter) WriteToBinFile() {
+	data := bf.ToBytes()
+
+	f, _ := os.OpenFile(BLOOM_FILE_PATH, os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+
+	f.Write(data)
+}
+
+func (bf *BloomFilter) LoadBloomFilter() {
+	f, _ := os.OpenFile(BLOOM_FILE_PATH, os.O_RDONLY, 0644)
+	defer f.Close()
+
+	stat, _ := f.Stat()
+
+	data := make([]byte, stat.Size())
+	f.Read(data)
+
+	bf.m = binary.BigEndian.Uint32(data[0:4])
+	bf.arr = data[4 : bf.m+4]
+	bf.k = binary.BigEndian.Uint32(data[bf.m+4 : bf.m+8])
+	offSet := bf.m + 8
+	bf.hf = make([]HashWithSeed, bf.k)
+	for i := 0; i < int(bf.k); i++ {
+		bf.hf[i] = HashWithSeed{Seed: data[offSet : offSet+32]}
+		offSet += 32
+	}
 }
 
 func Bloom() {
---
+++ bloom-filter/bloomfilter_const.go
@@ -0,0 +1,5 @@
+package bloom
+
+const (
+	BLOOM_FILE_PATH = "data/bloomfilter/bloomfilter.bin"
+)
