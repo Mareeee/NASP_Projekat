5ab27944da6b14321aad75329f2b58704dd3e40c
Igor Nikolic <igonik2003@gmail.com>
2023-11-29 18:02:30 +0100

    [UPDATE] Merkle Tree Implemented

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"main/merkle"
 )
 
 func main() {
-	fmt.Println("Napredni Algoritmi i Strukture Podataka!")
+	data := []string{"A", "B", "C", "D"}
+	mt := new(merkle.MerkleTree)
+	mt.MerkleTreeConstructor(data)
+	merkle.PrintTree(mt.Root, "")
+	fmt.Println("Root Hash:", mt.Root.Hash)
 }
