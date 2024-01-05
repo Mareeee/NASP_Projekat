cc99b39fe85ee42edb5142e7913594b15321c82e
Igor Nikolic <igonik2003@gmail.com>
2024-01-05 13:35:35 +0100

    [UPDATE] Serialization and Deserialization

--- main.go
+++ main.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
-	data := []string{"A", "B", "C", "D"}
-	mt := new(merkle.MerkleTree)
-	mt.MerkleTreeConstructor(data)
-	merkle.PrintTree(mt.Root, "")
-	fmt.Println("Root Hash:", mt.Root.Hash)
+	data := [][]byte{[]byte("mare"), []byte("dare"), []byte("care")}
+	merkleRoot := merkle.NewMerkleTree(data)
+
+	serijalizovano := merkle.SerializeMerkleTree(merkleRoot.Root)
+	deserialized := merkle.DeserializeMerkleTree(serijalizovano)
+
+	fmt.Println("\n[Original]")
+	merkle.PrintTree(merkleRoot.Root, "---")
+
+	fmt.Println("\n[Deserijalizovano] ")
+	merkle.PrintTree(deserialized, "  ")
+
+	// merkleRoot.WriteToBinFile("Merkle1")
+	// fmt.Println(merkle.ReadFromBinFile("data/merkle/Merkle1.bin"))
+
+	fmt.Println("\nDa li su isti??", merkle.CompareMerkleTrees(deserialized, merkleRoot.Root))
 }
