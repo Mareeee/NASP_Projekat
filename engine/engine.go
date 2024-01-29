2f064b27eb07b85ac71b5a48be1d0a1c4fb337b8
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2024-01-29 17:25:37 +0100

    [UPDATE] Menu

---
+++ engine/engine.go
@@ -0,0 +1,18 @@
+package engine
+
+import (
+	"main/config"
+)
+
+type Engine struct {
+	config config.Config
+}
+
+func (e *Engine) Put() {
+}
+
+func (e *Engine) Get() {
+}
+
+func (e *Engine) Delete() {
+}
