973a08cbc173ca5ba11e28a2f89165437908dceb
Marko Cvijanović <markocvijanovicmlg@gmail.com>
2024-01-29 16:55:21 +0100

    [UPDATE] Menu Initialization

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 package main
 
-// tBucket "main/tokenBucket"
+import "main/menu"
 
 func main() {
-	// wal := wal.LoadWal()
-	// wal.WriteRecord(*record.NewRecord("Mare", []byte("Zenta")))
-	// wal.WriteRecord(*record.NewRecord("Vlado", []byte("Zdravko")))
-	// wal.WriteRecord(*record.NewRecord("Segrecekic", []byte("Gic")))
-	// rekordi, _ := wal.LoadAllRecords()
-
-	// valueSlice := make([]record.Record, len(rekordi))
-	// for i, ptr := range rekordi {
-	// 	valueSlice[i] = *ptr
-	// }
-
-	// sstable.NewSSTable(valueSlice, 1)
-	// lsm.SizeTiered()
+	meni := menu.Menu{}
+	meni.Start()
 }
