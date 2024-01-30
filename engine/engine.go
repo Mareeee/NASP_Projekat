bffb1315cfc49514bff297b3b203bc6b0a6ee039
David Stakić <davidstakic2003@gmail.com>
2024-01-30 23:31:28 +0100

    [UPDATE] SimHash Menu

--- engine/engine.go
+++ engine/engine.go
@@ -1,7 +1,6 @@
 package engine
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"main/bloom-filter"
@@ -10,11 +9,10 @@ import (
 	"main/lsm"
 	"main/memtable"
 	"main/record"
+	"main/simhash"
 	"main/sstable"
 	tokenbucket "main/tokenBucket"
 	"main/wal"
-	"os"
-	"time"
 )
 
 type Engine struct {
@@ -140,78 +138,58 @@ func (e *Engine) Delete(key string) error {
 	return nil
 }
 
-func (e *Engine) BloomFilterOptions() {
-	fmt.Println("\n[1]	Create new instance")
-	fmt.Println("[2]	Delete instance")
-	fmt.Println("[3]	Add element")
-	fmt.Println("[4]	Search element")
+func (e *Engine) BloomFilterCreateNewInstance(key string) {
+	bloomFilter := bloom.NewBloomFilterMenu(100, 95.0)
+	value := bloomFilter.ToBytes()
+	fmt.Println(len(value))
+	e.Put("bf_"+key, value)
+}
 
-	optionScanner := bufio.NewScanner(os.Stdin)
-	optionScanner.Scan()
-	option := optionScanner.Text()
-
-	if e.Tbucket.Take() {
-		switch option {
-		case "1":
-			bloomFilter := bloom.NewBloomFilterMenu(100, 95.0)
-			value := bloomFilter.ToBytes()
-			fmt.Println(len(value))
-			key, _ := e.UserInput(false)
-			e.Put(key, value)
-		case "2":
-			key, _ := e.UserInput(false)
-			e.Delete(key)
-		case "3":
-			fmt.Println("Enter instance of bloomfilter: ")
-			key_bf, _ := e.UserInput(false)
-			bloom_record := e.Get(key_bf)
-			if bloom_record != nil {
-				fmt.Println("Enter element: ")
-				key, _ := e.UserInput(false)
-				bloomFilter := *bloom.FromBytes(bloom_record.Value)
-				bloomFilter.AddElement(key)
-				value := bloomFilter.ToBytes()
-				e.Put(key, value)
-			}
-		case "4":
-			fmt.Println("Enter instance of bloomfilter: ")
-			key_bf, _ := e.UserInput(false)
-			bloom_record := e.Get(key_bf)
-			if bloom_record != nil {
-				fmt.Println("Enter element: ")
-				key, _ := e.UserInput(false)
-				bloomFilter := *bloom.FromBytes(bloom_record.Value)
-				fmt.Println(bloomFilter.CheckElement(key))
-				value := bloomFilter.ToBytes()
-				e.Put(key, value)
-			}
-		default:
-			fmt.Println("Invalid option!")
-		}
-	} else {
-		fmt.Println("Rate limit exceeded. Waiting...")
-		time.Sleep(time.Second)
+func (e *Engine) BloomFilterAddElement(key, element string) {
+	bloom_record := e.Get(key)
+	if bloom_record != nil {
+		bloomFilter := *bloom.FromBytes(bloom_record.Value)
+		bloomFilter.AddElement(element)
+		value := bloomFilter.ToBytes()
+		e.Put(key, value)
 	}
 }
 
-func (e *Engine) UserInput(inputValueAlso bool) (string, []byte) {
-	fmt.Print("Input key: ")
-	keyScanner := bufio.NewScanner(os.Stdin)
-	keyScanner.Scan()
-	key := keyScanner.Text()
-	if key == "" {
-		fmt.Println("invalid key")
-		return "", nil
+func (e *Engine) BloomFilterCheckElement(key, element string) {
+	bloom_record := e.Get(key)
+	if bloom_record != nil {
+		bloomFilter := *bloom.FromBytes(bloom_record.Value)
+		fmt.Println(bloomFilter.CheckElement(element))
 	}
-	if inputValueAlso {
-		fmt.Print("Input value: ")
-		valueScanner := bufio.NewScanner(os.Stdin)
-		valueScanner.Scan()
-		value := valueScanner.Text()
-		return key, []byte(value)
-	} else {
-		return key, nil
+}
+
+func (e *Engine) CalculateFingerprintSimHash(key string, text string) error {
+	fingerprint := simhash.CalculateFingerprint(text)
+	value := simhash.ToBytes(fingerprint)
+	err := e.Put(key, value)
+	if err != nil {
+		return err
 	}
+	fmt.Println("fingerprint = " + string(value))
+	return nil
+}
+
+func (e *Engine) CalculateHammingDistanceSimHash(key1, key2 string) error {
+	record1 := e.Get(key1)
+	if record1 == nil {
+		return errors.New("key not found")
+	}
+	record2 := e.Get(key2)
+	if record2 == nil {
+		return errors.New("key not found")
+	}
+
+	fingerprint1 := simhash.LoadFromBytes(record1.Value)
+	fingerprint2 := simhash.LoadFromBytes(record2.Value)
+	hamming := simhash.HammingDistance(fingerprint1, fingerprint2)
+	fmt.Println("hamming distance = " + fmt.Sprint(hamming))
+
+	return nil
 }
 
 func (e *Engine) recover() error {
